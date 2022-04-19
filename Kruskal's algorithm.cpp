0ef3dd98dce2f07339dc95eb7f572cc99deabbf3
sourav-gt47 <83998155+sourav-gt47@users.noreply.github.com>
2022-04-19 23:13:08 +0530

    Update Kruskal's algorithm.cpp

--- Kruskal's algorithm.cpp
+++ Kruskal's algorithm.cpp
@@ -1,105 +1,90 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-// DSU data structure
-// path compression + rank by union
+int n, m;
 
-class DSU {
-	int* parent;
-	int* rank;
+struct graph{
+int a;  //leftmost point of edge
+int b;  //rightmost point of edge
+int weight; // weight of edge
 
-public:
-	DSU(int n)
-	{
-		parent = new int[n];
-		rank = new int[n];
-
-		for (int i = 0; i < n; i++) {
-			parent[i] = -1;
-			rank[i] = 1;
-		}
-	}
-
-	// Find function
-	int find(int i)
-	{
-		if (parent[i] == -1)
-			return i;
-
-		return parent[i] = find(parent[i]);
-	}
-	// union function
-	void unite(int x, int y)
-	{
-		int s1 = find(x);
-		int s2 = find(y);
-
-		if (s1 != s2) {
-			if (rank[s1] < rank[s2]) {
-				parent[s1] = s2;
-				rank[s2] += rank[s1];
-			}
-			else {
-				parent[s2] = s1;
-				rank[s1] += rank[s2];
-			}
-		}
-	}
 };
+graph arr[100];
+int parent[100];
 
-class Graph {
-	vector<vector<int> > edgelist;
-	int V;
 
-public:
-	Graph(int V) { this->V = V; }
-
-	void addEdge(int x, int y, int w)
-	{
-		edgelist.push_back({ w, x, y });
-	}
-
-	void kruskals_mst()
-	{
-		// 1. Sort all edges
-		sort(edgelist.begin(), edgelist.end());
-
-		// Initialize the DSU
-		DSU s(V);
-		int ans = 0;
-		cout << "Following are the edges in the "
-				"constructed MST"
-			<< endl;
-		for (auto edge : edgelist) {
-			int w = edge[0];
-			int x = edge[1];
-			int y = edge[2];
-
-			// take that edge in MST if it does form a cycle
-			if (s.find(x) != s.find(y)) {
-				s.unite(x, y);
-				ans += w;
-				cout <<char(x + 65) << " -- " << char(y + 65) << " == " << w
-					<< endl;
-			}
-		}
-		cout << "Minimum Cost Spanning Tree: " << ans;
-	}
-};
-int main()
+bool capacity(struct graph a , struct graph b )
 {
 
+return (a.weight < b.weight);
 
-	 int n, m;
-	 cin >> n >> m;
 
-	 Graph g(n);
-	 for (int i = 0; i < m; i++)
-	 {
-		 int x, y, w;
-		 cin >> x >> y >> w;
-		 g.addEdge(x, y, w);
-	 }
+}
+// find parent function
 
-	g.kruskals_mst();
-	return 0;
+int find (int i){
+while(parent[i] > 0){
+    i = parent[i];
+    
+}
+return i;
+}
+
+//find union function
+
+void uni_on(int i , int j){
+
+int count = parent[i] + parent[j];
+
+if(parent[i] <= parent[j]){
+
+parent[j] = i;
+parent[i] = count;
+}
+
+else{
+parent[i] = j;
+parent[j] = count;
+
+}
+
+}
+
+
+
+
+
+int main()
+{
+int sum=0;
+cin>>n;
+cin>>m;
+graph arr[n];
+int parent[m];
+for(int i=1;i<=n; i++ ) parent[i] = -1;
+
+for(int i=0; i<m; i++)
+{
+  cin>>arr[i].a>>arr[i].b>>arr[i].weight;
+}
+
+sort (arr, arr+m,capacity);
+
+
+for(int i=0; i<m; i++){
+
+int a = find(arr[i].a);
+int b = find(arr[i].b);
+
+if (a != b)
+{
+sum+= arr[i].weight;
+uni_on(a , b);
+cout<<char(a + 64)<<"--"<<char(b + 64)<<"  "<<arr[i].weight<<endl;
+}
+
+}
+
+cout<<"Total profit::"<<sum<<endl;
+
+    return 0;
 }
